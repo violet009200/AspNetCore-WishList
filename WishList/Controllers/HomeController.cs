603e2ffbb4001ca759e8583a0c0e8fd39f3ffe44
violet009200 <violet009200@gmail.com>
2020-03-01 13:59:04 -0600

    -

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -6,19 +6,19 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace HomeController
+namespace WishList.Controllers
 {
     public class HomeController : Controller
     {
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public IActionResult Error()
         {
-            return View();
+            return View("Error");
         }
     }
 }
