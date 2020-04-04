b06f9ede127d6088fe2db2c667295f33aae7b20c
violet009200 <violet009200@gmail.com>
2020-04-04 15:03:54 -0500

    added Item class

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id;
+
+        [MaxLength(50)]
+        public string Description;
+
+        public Item() { }
+
+    }
+}
