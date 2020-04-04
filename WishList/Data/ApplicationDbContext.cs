ff3671651f4845fbfcb4fe75d490c91e490e7cdd
violet009200 <violet009200@gmail.com>
2020-04-04 14:58:44 -0500

    Create Item Model With EntityFramework Support

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) :base(options)
+        {
+  
+        }
+    }
+}
--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WishList.Data;
 
 namespace WishList
 {
@@ -19,6 +20,8 @@ namespace WishList
         {
             //In the ConfigureServices method, call AddMvc on services to add support for MVC middleware.
             services.AddMvc();
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("WishList"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
